ad2e49c7e33cd7f61abcc01da4cc11420502ce3f
David Lesches <david@lesches.com>
2013-08-27 11:42:37 -0400

    Update

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,6 @@
 
 env :PATH, '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin'
 
-every %w(sun mon tue wed thu sat), :at => '11:30 am' do
-  runner "Report.create_for_today"
-end
-
 every %w(sun mon tue wed thu sat), :at => '11:00 pm' do
   runner "Report.create_for_today"
 end
