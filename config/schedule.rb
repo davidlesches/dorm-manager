7970e30e593067cbd6a4f9067621028fa0cda90d
David Lesches <david@lesches.com>
2015-04-22 17:30:07 -0400

    change

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,6 @@ every %w(sun mon tue wed thu sat), :at => '11:00 pm' do
   runner "Report.create_for_today"
 end
 
-every %w(sun mon tue wed thu fri), :at => '1:30 am' do
+every %w(sun mon tue wed thu fri), :at => '2:00 am' do
   runner "Report.daily_work"
 end
