e4778677448288be9b2b9422f7a44c4a3ee39dd8
David Lesches <david@lesches.com>
2016-04-15 10:36:02 -0400

    Turn off

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
 
 env :PATH, '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin'
 
-every %w(sun mon tue wed thu sat), :at => '11:00 pm' do
-  runner "Report.create_for_today"
-end
-
-every %w(sun mon tue wed thu fri), :at => '2:00 am' do
-  runner "Report.daily_work"
-end
+# every %w(sun mon tue wed thu sat), :at => '11:00 pm' do
+#   runner "Report.create_for_today"
+# end
+#
+# every %w(sun mon tue wed thu fri), :at => '2:00 am' do
+#   runner "Report.daily_work"
+# end
