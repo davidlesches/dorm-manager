0786acec10544b8282bdaace349c75c713e37633
David Lesches <david@lesches.com>
2017-09-05 13:47:03 -0400

    Update emails

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,10 +2,11 @@ class Mailer < ActionMailer::Base
   default from: 'report@dorm.tsot.edu'
 
   def report options
+    emails = 'lesches01@gmail.com, kervelten@gmail.com, info@chcycles.com, rndeitsch@oholeitorah.com'
     @out = options[:out]
     @not_logged = options[:not_logged]
     @reports = options[:reports]
-    mail :to => 'lesches01@gmail.com, kervelten@gmail.com, info@chcycles.com', :subject => "Dorm Report - #{Time.zone.now.strftime("%B %-d")}"
+    mail :to => emails, :subject => "Dorm Report - #{Time.zone.now.strftime("%B %-d")}"
   end
 
 end
