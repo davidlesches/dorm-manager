f2e95bb7010e4b47b6623c56637f41d7d927b184
David Lesches <david@lesches.com>
2017-10-16 21:26:17 -0400

    Additional email

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -5,7 +5,7 @@ class Mailer < ActionMailer::Base
     @out = options[:out]
     @not_logged = options[:not_logged]
     @reports = options[:reports]
-    mail :to => 'lesches01@gmail.com, kervelten@gmail.com, info@chcycles.com', :subject => "Dorm Report - #{Time.zone.now.strftime("%B %-d")}"
+    mail :to => 'lesches01@gmail.com, kervelten@gmail.com, info@chcycles.com, rndeitsch@oholeitorah.com', :subject => "Dorm Report - #{Time.zone.now.strftime("%B %-d")}"
   end
 
 end
